e87474e14cfdad648ffe017e2876cc3225d258ff
Agastya8543 <128040809+Agastya8543@users.noreply.github.com>
2023-11-11 20:59:48 +0530

    Update L7A2.py

--- L7A2.py
+++ L7A2.py
@@ -22,9 +22,12 @@ support_vectors = clf.support_vectors_
 # Print the support vectors
 print(f'Support Vectors ={support_vectors}')
 
-
-accuracy = clf.score(X_train, y_train)
-
-# Print the accuracy
-print(f"Accuracy: {accuracy:.2f}")
-
+# Testing the accuracy of the SVM on the test set
+accuracy = clf.score(X_test[['embed_0', 'embed_1']], y_test)
+print(f"Accuracy of the SVM on the test set: {accuracy}")
+ 
+# Perform classification for the given test vector
+test_vector = X_test[['embed_0', 'embed_1']].iloc[0]
+predicted_class = clf.predict([test_vector])
+print(f"The predicted class for the test vector: {predicted_class}")
+ 
